ac1584f840b66fd1953e6e1ea99317c85b5403b6
fares takorabt <farestakorabt@gmail.com>
2022-03-03 23:31:06 +0100

    last change

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,9 +4,8 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <div className="navigation">
-        
       <ul>
-        <NavLink to="/" className="hover" activenclassname="nav-active">
+        <NavLink to="/" className="hover" activeClassName="nav-active">
           <li>Accueil</li>
         </NavLink>
         <li className="nav-portfolio">
@@ -14,38 +13,37 @@ const Navigation = () => {
           <ul className="nav-projects">
             <NavLink
               to="/project1"
-              activenclassname="nav-active"
+              activeClassName="nav-active"
               className="hover"
             >
               <li>Projet 1</li>
             </NavLink>
             <NavLink
               to="/project2"
-              activenclassname="nav-active"
+              activeClassName="nav-active"
               className="hover"
             >
               <li>Projet 2</li>
             </NavLink>
             <NavLink
               to="/project3"
-              activenclassname="nav-active"
+              activeClassName="nav-active"
               className="hover"
             >
               <li>Projet 3</li>
             </NavLink>
             <NavLink
               to="/project4"
-              activenclassname="nav-active"
+              activeClassName="nav-active"
               className="hover"
             >
               <li>Projet 4</li>
             </NavLink>
           </ul>
         </li>
-        <NavLink to="/contact" className="hover" activeclassname="nav-active">
+        <NavLink to="/contact" className="hover" activeClassName="nav-active">
           Contact
         </NavLink>
-
       </ul>
     </div>
   );
